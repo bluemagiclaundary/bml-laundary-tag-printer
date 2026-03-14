da005159cfe34abdd389b96dcb26126d94686f02
bluemagiclaundary <bluemagiclaundary@gmail.com>
2026-03-15 01:18:32 +0530

    Add tag generation and printing functions

---
+++ script.js
@@ -0,0 +1,39 @@
+function generateTags(){
+
+let orderNo=document.getElementById("orderNo").value
+let customer=document.getElementById("customer").value
+let wash=document.getElementById("wash").value
+let delivery=document.getElementById("delivery").value
+let qty=parseInt(document.getElementById("qty").value)
+let starchQty=parseInt(document.getElementById("starchQty").value)
+
+let container=document.getElementById("tags")
+container.innerHTML=""
+
+for(let i=1;i<=qty;i++){
+
+let starchText=""
+
+if(i<=starchQty){
+starchText="STARCH"
+}
+
+let tag=document.createElement("div")
+tag.className="tag"
+
+tag.innerHTML=`<b>BML</b><br>
+${orderNo}<br>
+${customer}<br>
+${wash}<br>
+${starchText}
+${delivery}<br> <b>${i}/${qty}</b>`
+
+container.appendChild(tag)
+
+}
+
+}
+
+function printTags(){
+window.print()
+}
