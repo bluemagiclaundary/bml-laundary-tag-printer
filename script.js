5c4903f89abfa4a07fc4982ae9c9eacfa827ec08
bluemagiclaundary <bluemagiclaundary@gmail.com>
2026-03-15 22:57:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,56 +2,77 @@ function generateTags(){
 
 let orderNo=document.getElementById("orderNo").value
 let customer=document.getElementById("customer").value
-let wash=document.getElementById("wash").value
 let delivery=document.getElementById("delivery").value
-let qty=parseInt(document.getElementById("qty").value)
-let starchQty=parseInt(document.getElementById("starchQty").value)
+
+let dcQty=parseInt(document.getElementById("dcQty").value)||0
+let dcStarchQty=parseInt(document.getElementById("dcStarchQty").value)||0
+let dcStarchType=document.getElementById("dcStarchType").value
+
+let wiQty=parseInt(document.getElementById("wiQty").value)||0
+let wiStarchQty=parseInt(document.getElementById("wiStarchQty").value)||0
+let wiStarchType=document.getElementById("wiStarchType").value
+
+let wfQty=parseInt(document.getElementById("wfQty").value)||0
+let siQty=parseInt(document.getElementById("siQty").value)||0
+
+let garments=[]
+
+for(let i=0;i<dcQty;i++){
+garments.push({wash:"DC", starch:i<dcStarchQty ? dcStarchType : ""})
+}
+
+for(let i=0;i<wiQty;i++){
+garments.push({wash:"WI", starch:i<wiStarchQty ? wiStarchType : ""})
+}
+
+for(let i=0;i<wfQty;i++){
+garments.push({wash:"WF", starch:""})
+}
+
+for(let i=0;i<siQty;i++){
+garments.push({wash:"SI", starch:""})
+}
+
+let total=garments.length
 
 let container=document.getElementById("tags")
 container.innerHTML=""
 
-for(let i=1;i<=qty;i++){
+garments.forEach((g,index)=>{
 
 let starchLine=""
-
-if(i<=starchQty){
-starchLine="<div>STARCH</div>"
+if(g.starch){
+starchLine="<div>STARCH "+g.starch+"</div>"
 }
 
 let tag=document.createElement("div")
 tag.className="tag"
 
-tag.innerHTML=
-`
+tag.innerHTML=`
 
 <div><b>BML</b></div>
 <div>${orderNo}</div>
 <div>${customer}</div>
-<div>${wash}</div>
+<div>${g.wash}</div>
 ${starchLine}
-<div><b>${i}/${qty}</b></div>
+<div><b>${index+1}/${total}</b></div>
 <div>${delivery}</div>
 `
 
 container.appendChild(tag)
 
-}
+})
 
 }
 
 function printTags(){
 window.print()
 }
-/* REFRESH BUTTON FUNCTION */
 
 function refreshPage(){
 
-document.getElementById("orderNo").value=""
-document.getElementById("customer").value=""
-document.getElementById("wash").value="DC"
-document.getElementById("delivery").value=""
-document.getElementById("qty").value=""
-document.getElementById("starchQty").value="0"
-
+document.querySelectorAll("input").forEach(i=>i.value="")
+document.querySelectorAll("select").forEach(s=>s.selectedIndex=0)
 document.getElementById("tags").innerHTML=""
-}  
+
+}
